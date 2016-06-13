70a7f9f0b7f0b2bd3d3ad460b8ca2ee01a7f0a17
Tom Wanzek <tomwanzek@gmail.com>
2016-06-13 14:38:35 -0400

    Installed dev dependencies (npm). Added grunt support for comile tasks. Added vscode related settings. Updated gitignore to exclude compiled test files. Initialized tsconfig.json.

---
+++ Gruntfile.js
@@ -0,0 +1,51 @@
+module.exports = function (grunt) {
+
+    grunt.initConfig({
+
+        pkg: grunt.file.readJSON('package.json'),
+
+        // Housekeeping Tasks --------------------------
+
+        clean: {
+            source: ['tests/**/*.js', 'tests/**/*.js.map']
+        },
+
+        // Compile Tasks -------------------------------
+        ts: {
+            compileService: {
+                files: [{
+                    src: [
+                        'tests/**/*.ts',
+                        'typings/index.d.ts'
+                    ]
+                }],
+                options: {
+                    module: 'commonjs',
+                    target: 'es5',
+                    fast: 'never',
+                    moduleResolution: 'node',
+                    removeComments: false,
+                    declaration: false,
+                    sourceMap: false,
+                    noImplicitAny: true
+                }
+            }
+        }
+    });
+
+    // -----------------------------------------------------------------
+    // Load Grunt Plugins
+    // -----------------------------------------------------------------
+
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-ts');
+
+
+    // -----------------------------------------------------------------
+    // Transpile from Typescript to target format JS
+    // -----------------------------------------------------------------
+
+    grunt.registerTask('compile', ['clean:source', 'ts:compileService']);
+
+
+};
