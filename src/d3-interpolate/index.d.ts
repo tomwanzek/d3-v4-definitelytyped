9ab07dfa466281ca8487308e9192f3b1dfe39a25
Tom Wanzek <tomwanzek@gmail.com>
2016-06-15 14:11:25 -0400

    Added interpolate definitions with definitions test.

---
+++ src/d3-interpolate/index.d.ts
@@ -0,0 +1,94 @@
+// Type definitions for d3JS d3-interpolate module
+// Project: http://d3js.org/
+// Definitions by: Alex Ford <https://github.com/gustavderdrache>, Boris Yankov <https://github.com/borisyankov>, Tom Wanzek <https://github.com/tomwanzek>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+import * as d3_color from 'd3-color';
+
+
+// --------------------------------------------------------------------------
+// Shared Type Definitions and Interfaces
+// --------------------------------------------------------------------------
+
+/**
+ * Type for Interpolation Function
+ */
+export interface InterpolationFn<T> extends Function {
+    (t: number): T;
+}
+
+export interface ZoomInterpolationFn extends InterpolationFn<ZoomView> {
+    /**
+     * Recommended duration of zoom transition in ms
+     */
+    duration: number;
+}
+
+export interface ColorGammaInterpolationFactory extends Function {
+    (a: string | d3_color.ColorSpaceObject, b: string | d3_color.ColorSpaceObject): InterpolationFn<string>;
+    gamma(g: number): ColorGammaInterpolationFactory;
+}
+
+/**
+ * Type zoomView is used to represent a numeric array with three elements.
+ * In order of appearance the elements correspond to:
+ * - cx: x-coordinate of the center of the viewport
+ * - cy: y-coordinate of the center of the viewport
+ * - width: size of the viewport
+ */
+export type ZoomView = [number, number, number];
+
+// --------------------------------------------------------------------------
+// Interpolation Function Factories
+// --------------------------------------------------------------------------
+
+export function interpolate(a: any, b: null): InterpolationFn<null>;
+export function interpolate(a: any, b: number): InterpolationFn<number>;
+export function interpolate(a: any, b: d3_color.ColorSpaceObject): InterpolationFn<string>;
+export function interpolate(a: any, b: string): InterpolationFn<string>;
+export function interpolate<U>(a: any, b: Array<U>): InterpolationFn<Array<U>>;
+export function interpolate(a: any, b: { [key: string]: any }): InterpolationFn<{ [key: string]: any }>;
+export function interpolate<U extends Object>(a: any, b: U): InterpolationFn<U>;
+
+export function interpolateNumber(a: number, b: number): InterpolationFn<number>;
+
+export function interpolateRound(a: number, b: number): InterpolationFn<number>;
+
+export function interpolateString(a: string, b: string): InterpolationFn<string>;
+
+export function interpolateArray<U>(a: Array<any>, b: Array<U>): InterpolationFn<Array<U>>;
+
+export function interpolateObject(a: { [key: string]: any }, b: { [key: string]: any }): InterpolationFn<{ [key: string]: any }>;
+export function interpolateObject<U extends Object>(a: any, b: U): InterpolationFn<U>;
+
+
+export function interpolateTransformCss(a: string, b: string): InterpolationFn<string>;
+export function interpolateTransformSvg(a: string, b: string): InterpolationFn<string>;
+
+/**
+ * Create Interpolator for zoom views
+ */
+export function interpolateZoom(a: ZoomView, b: ZoomView): ZoomInterpolationFn;
+
+
+export function quantize<T>(interpolator: InterpolationFn<T>, n :number): Array<T>;
+
+// Color interpolation related
+
+export var interpolateRgb: ColorGammaInterpolationFactory;
+
+export function interpolateRgbBasis(colors: Array<string | d3_color.ColorSpaceObject>): InterpolationFn<string>;
+export function interpolateRgbBasisClosed(colors: Array<string | d3_color.ColorSpaceObject>): InterpolationFn<string>;
+
+export function interpolateHsl(a: string | d3_color.ColorSpaceObject, b: string | d3_color.ColorSpaceObject): InterpolationFn<string>;
+export function interpolateHslLong(a: string | d3_color.ColorSpaceObject, b: string | d3_color.ColorSpaceObject): InterpolationFn<string>;
+export function interpolateLab(a: string | d3_color.ColorSpaceObject, b: string | d3_color.ColorSpaceObject): InterpolationFn<string>;
+export function interpolateHcl(a: string | d3_color.ColorSpaceObject, b: string | d3_color.ColorSpaceObject): InterpolationFn<string>;
+export function interpolateHclLong(a: string | d3_color.ColorSpaceObject, b: string | d3_color.ColorSpaceObject): InterpolationFn<string>;
+export var interpolateCubehelix: ColorGammaInterpolationFactory;
+export var interpolateCubehelixLong: ColorGammaInterpolationFactory;
+
+// Spline related
+
+export function interpolateBasis(splineNodes: Array<number>): InterpolationFn<number>;
+export function interpolateBasisClosed(splineNodes: Array<number>): InterpolationFn<number>;
---
+++ tests/d3-interpolate/d3-interpolate-test.ts
@@ -0,0 +1,170 @@
+/**
+ * Typescript definition tests for d3/d3-ease module
+ * 
+ * Note: These tests are intended to test the definitions only
+ * in the sense of typing and call signature consistency. They
+ * are not intended as functional tests.
+ */
+
+import * as d3Color from 'd3-color';
+import * as d3Interpolate from 'd3-interpolate';
+
+// Preparatory steps -------------------------------------------------------------------
+
+let iNum: d3Interpolate.InterpolationFn<number>,
+    iString: d3Interpolate.InterpolationFn<string>,
+    iArrayNum: d3Interpolate.InterpolationFn<Array<number>>,
+    iArrayStr: d3Interpolate.InterpolationFn<Array<string>>,
+    iKeyVal: d3Interpolate.InterpolationFn<{ [key: string]: any }>,
+    iRGBColorObj: d3Interpolate.InterpolationFn<d3Color.RGBColor>,
+    iZoom: d3Interpolate.ZoomInterpolationFn;
+
+let num: number,
+    str: string,
+    arrNum: number[],
+    arrStr: string[],
+    objKeyVal: { [key: string]: any },
+    objRGBColor: d3Color.RGBColor,
+    zoom: [number, number, number];
+
+// test interpolate(a, b) signature ----------------------------------------------------
+
+iNum = d3Interpolate.interpolate('1', 5);
+
+
+// color interpolator returning a color string
+iString = d3Interpolate.interpolate('seagreen', d3Color.rgb(100, 100, 100));
+iString = d3Interpolate.interpolate('seagreen', 'steelblue'); // as used with valid color name string
+
+// regular string interpolator interpolating number strings (as the strings are not valid color strings)
+iString = d3Interpolate.interpolate(1, '5');
+
+iArrayNum = d3Interpolate.interpolate(['1', '2'], [4, 8]);
+iArrayStr = d3Interpolate.interpolate(['1', '2'], ['4', '8']);
+
+iKeyVal = d3Interpolate.interpolate({ x: 0, y: 1 }, { x: 1, y: 10, z: 100 });
+
+
+// test interpolateNumber(a, b) signature ----------------------------------------------------
+
+iNum = d3Interpolate.interpolateNumber(0, 100);
+num = iNum(0.5);
+
+// test interpolateNumber(a, b) signature ----------------------------------------------------
+
+iNum = d3Interpolate.interpolateRound(0, 100);
+num = iNum(0.5);
+
+// test interpolateString(a, b) signature ----------------------------------------------------
+
+iString = d3Interpolate.interpolateString('-1', '2');
+str = iString(0.5);
+
+// test interpolateArray(a, b) signature ----------------------------------------------------
+
+iArrayNum = d3Interpolate.interpolateArray<number>([1, 2], [4, 8]); // explicit typing
+arrNum = iArrayNum(0.5);
+
+iArrayNum = d3Interpolate.interpolateArray<number>(['1', '2'], [4, 8]); // explicit typing
+arrNum = iArrayNum(0.5);
+
+iArrayStr = d3Interpolate.interpolateArray<string>(['1', '2'], ['4', '8']); // explicit typing
+arrStr = iArrayStr(0.5);
+
+iArrayStr = d3Interpolate.interpolateArray([1, 2], ['4', '8']); // infered typing <string>
+arrStr = iArrayStr(0.5);
+
+// test interpolateObject(a, b) signature ----------------------------------------------------
+
+iKeyVal = d3Interpolate.interpolateObject({ x: 0, y: 1 }, { x: 1, y: 10, z: 100 });
+objKeyVal = iKeyVal(0.5);
+
+iRGBColorObj = d3Interpolate.interpolateObject<d3Color.RGBColor>(d3Color.rgb('steelblue'), d3Color.rgb('seagreen'));
+objRGBColor = iRGBColorObj(0.5);
+
+// test interpolateTransformCss(a, b) signature ----------------------------------------------------
+
+iString = d3Interpolate.interpolateTransformCss('rotate(0deg)', 'rotate(60deg)');
+str = iString(0.5);
+
+// test interpolateTransformSvg(a, b) signature ----------------------------------------------------
+
+iString = d3Interpolate.interpolateTransformSvg('rotate(0)', 'rotate(60)');
+str = iString(0.5);
+
+
+// test interpolateZoom(a, b) signature ----------------------------------------------------
+
+iZoom = d3Interpolate.interpolateZoom([50, 50, 300], [100, 100, 500]);
+zoom = iZoom(0.5);
+
+console.log('Recommended transition duration = %d', iZoom.duration);
+
+// test quantize(interpolator, n) signature ------------------------------------------------
+
+arrNum = d3Interpolate.quantize(d3Interpolate.interpolateRound(-1, 2), 4); // infered template parameter type
+arrStr = d3Interpolate.quantize<string>(d3Interpolate.interpolateString('-1', '2'), 4); // explicit template parameter typing
+
+// arrStr = d3Interpolate.quantize<string>(d3Interpolate.interpolateRound(-1, 2), 4); // test fails, due to explicit typing v argument type mismatch
+
+// test interpolateRgb(a, b) signatures ----------------------------------------------------------------
+
+// without gamma correction
+iString = d3Interpolate.interpolateRgb('seagreen', 'steelblue');
+iString = d3Interpolate.interpolateRgb(d3Color.rgb('seagreen'), d3Color.hcl('steelblue'));
+str = iString(0.5);
+
+// with gamma correction
+iString = d3Interpolate.interpolateRgb.gamma(2.2)("purple", "orange");
+
+// test interpolateRgbBasis(color) and  interpolateRgbBasisClosed(color) signatures -------------------------
+
+iString = d3Interpolate.interpolateRgbBasis(['seagreen', d3Color.rgb('steelblue'), 'rgb(100, 100, 100)']);
+iString = d3Interpolate.interpolateRgbBasisClosed(['seagreen', d3Color.rgb('steelblue'), 'rgb(100, 100, 100)']);
+
+// test interpolateHsl(a, b) and interpolateHslLong(a, b)----------------------------------------------------------------
+
+iString = d3Interpolate.interpolateHsl('seagreen', 'steelblue');
+iString = d3Interpolate.interpolateHsl(d3Color.rgb('seagreen'), d3Color.hcl('steelblue'));
+
+iString = d3Interpolate.interpolateHslLong('seagreen', 'steelblue');
+iString = d3Interpolate.interpolateHslLong(d3Color.rgb('seagreen'), d3Color.hcl('steelblue'));
+
+// test interpolateLab(a, b) --------------------------------------------------------------------------------------------
+
+iString = d3Interpolate.interpolateLab('seagreen', 'steelblue');
+iString = d3Interpolate.interpolateLab(d3Color.rgb('seagreen'), d3Color.hcl('steelblue'));
+
+// test interpolateHcl(a, b) and interpolateHclLong(a, b) ----------------------------------------------------------------
+
+iString = d3Interpolate.interpolateHcl('seagreen', 'steelblue');
+iString = d3Interpolate.interpolateHcl(d3Color.rgb('seagreen'), d3Color.hcl('steelblue'));
+
+iString = d3Interpolate.interpolateHclLong('seagreen', 'steelblue');
+iString = d3Interpolate.interpolateHclLong(d3Color.rgb('seagreen'), d3Color.hcl('steelblue'));
+
+// test interpolateCubehelix(a, b) and interpolateCubehelixLong(a, b) ---------------------------------------------------
+
+// without gamma correction
+iString = d3Interpolate.interpolateCubehelix('seagreen', 'steelblue');
+iString = d3Interpolate.interpolateCubehelix(d3Color.rgb('seagreen'), d3Color.hcl('steelblue'));
+str = iString(0.5);
+
+// with gamma correction
+iString = d3Interpolate.interpolateCubehelix.gamma(2.2)("purple", "orange");
+
+// without gamma correction
+iString = d3Interpolate.interpolateCubehelixLong('seagreen', 'steelblue');
+iString = d3Interpolate.interpolateCubehelixLong(d3Color.rgb('seagreen'), d3Color.hcl('steelblue'));
+str = iString(0.5);
+
+// with gamma correction
+iString = d3Interpolate.interpolateCubehelixLong.gamma(2.2)("purple", "orange");
+
+
+
+// test interpolateBasis(splineNodes) and interpolateBasisClosed(splineNodes: Array<number>) ----------------------------
+
+iNum = d3Interpolate.interpolateBasis([1, 50, 30, 10]);
+
+iNum = d3Interpolate.interpolateBasisClosed([1, 50, 30, 10]);
