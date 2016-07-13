0ab1d36c1eca763768627a92d2b78979d9bfbe80
Tom Wanzek <tomwanzek@gmail.com>
2016-07-12 23:03:27 -0400

    d3-polygon: * fixed typo in argument name (purely cosmetic) * added completed tests. Fixes #16.

--- src/d3-polygon/index.d.ts
+++ src/d3-polygon/index.d.ts
@@ -28,7 +28,7 @@ export function polygonCentroid(polygon: Array<[number, number]>): [number, numb
  * 
  * @param points Array of coordinates <x0, y0>, <x1, y1> and so on.
  */
-export function polygonHull(point: Array<[number, number]>): Array<[number, number]> | null;
+export function polygonHull(points: Array<[number, number]>): Array<[number, number]> | null;
 
 /**
  * Returns true if and only if the specified point is inside the specified polygon.
--- tests/d3-polygon/d3-polygon-test.ts
+++ tests/d3-polygon/d3-polygon-test.ts
@@ -8,4 +8,27 @@
 
 import * as d3Polygon from '../../src/d3-polygon';
 
-// TODO: Add definitions tests
+// -----------------------------------------------------------------------------
+// Preparatory Steps
+// -----------------------------------------------------------------------------
+
+let num: number;
+let containsFlag: boolean;
+let point: [number, number] = [15, 15];
+let polygon: Array<[number, number]> = [[10, 10], [20, 20], [10, 30]];
+let pointArray: Array<[number, number]> = [[10, 10], [20, 20], [10, 30], [15, 15]];
+let hull: Array<[number, number]>;
+
+// -----------------------------------------------------------------------------
+// Tests
+// -----------------------------------------------------------------------------
+
+num = d3Polygon.polygonArea(polygon);
+
+point = d3Polygon.polygonCentroid(polygon);
+
+hull = d3Polygon.polygonHull(pointArray);
+
+containsFlag = d3Polygon.polygonContains(polygon, point);
+
+num = d3Polygon.polygonLength(polygon);
