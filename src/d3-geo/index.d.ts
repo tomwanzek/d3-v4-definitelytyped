604a2d7f058e12fbdc43a8cea744bcec764c1039
Hugues Stefanski <hugues.stefanski@gmail.com>
2016-07-27 15:04:46 +0200

    Merged d3-geo header

--- src/d3-geo/index.d.ts
+++ src/d3-geo/index.d.ts
@@ -1,6 +1,59 @@
 // Type definitions for D3JS d3-geo module 1.1.1
 // Project: https://github.com/d3/d3-geo/
-// Definitions by: Alex Ford <https://github.com/gustavderdrache>, Boris Yankov <https://github.com/borisyankov>
+// Definitions by: Alex Ford <https://github.com/gustavderdrache>, Boris Yankov <https://github.com/borisyankov>, Hugues Stefanski <https://github.com/Ledragon>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-// TODO: Write definitions
+export interface GeoPath {
+    area(object: any): number;
+    bounds(object: any): [[number, number], [number, number]];
+    centroid(object: any): [number, number];
+    projection(): Projection;
+    projection(projection: Projection): this;
+    (object: any): string;
+}
+
+export interface Projection {
+    (point: [number, number]): [number, number];
+
+    clipAngle(): number;
+    clipAngle(angle?: number): this;
+
+    scale(): number;
+    scale(scale?: number): this;
+
+    translate(): [number, number];
+    translate(point: [number, number]): this;
+
+    center(): [number, number];
+    center(point: [number, number]): this;
+
+    invert(point: [number, number]): [number, number];
+    stream(stream: any): ProjectionStream;
+}
+
+export interface ProjectionStream {
+}
+
+export interface RawProjection {
+    (longitude: number, latitude: number): [number, number];
+    invert(x: number, y: number): [number, number];
+}
+
+export function geoPath(): GeoPath;
+export function geoProjection(project: RawProjection): Projection;
+
+export function geoAzimuthalEqualAreaRaw(): RawProjection;
+export function geoAzimuthalEquidistantRaw(): RawProjection;
+export function geoConicConformalRaw(phi0: number, phi1: number): RawProjection;
+export function geoConicEqualAreaRaw(phi0: number, phi1: number): RawProjection;
+export function geoConicEquidistantRaw(phi0: number, phi1: number): RawProjection;
+export function geoEquirectangularRaw(): RawProjection;
+export function geoGnomonicRaw(): RawProjection;
+export function geoMercatorRaw(): RawProjection;
+export function geoOrthographicRaw(): RawProjection;
+export function geoStereographicRaw(): RawProjection;
+export function geoTransverseMercatorRaw(): RawProjection;
+
+export function geoAlbers(): Projection;
+export function geoAlbersUsa(): Projection;
+export function geoMercator(): Projection;
