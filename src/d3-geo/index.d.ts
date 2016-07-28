bec811185b10284b9afca133f86bdc188838d034
Hugues Stefanski <hugues.stefanski@gmail.com>
2016-07-28 17:31:47 +0200

    d3-geo WIP

--- src/d3-geo/index.d.ts
+++ src/d3-geo/index.d.ts
@@ -5,6 +5,17 @@
 
 /// <reference path="typings/browser.d.ts" />
 
+export interface CircleGenerator {
+    /**Returns a new GeoJSON geometry object of type “Polygon” approximating a circle on the surface of a sphere, with the current center, radius and precision. */
+    (...args: any[]): GeoJSON.Polygon;
+    center(): ((...args: any[]) => [number, number]) | [number, number];
+    center(center: ((...args: any[]) => [number, number]) | [number, number]): this;
+    radius(): ((...args: any[]) => number) | number;
+    radius(radius: ((...args: any[]) => number) | number): this;
+    precision(): ((...args: any[]) => number) | number;
+    precision(precision: ((...args: any[]) => number) | number): this;
+}
+
 export interface Projection {
     (point: [number, number]): [number, number];
 
@@ -24,7 +35,68 @@ export interface Projection {
     stream(stream: any): ProjectionStream;
 }
 
+export interface ConicProjection extends Projection{
+    //TODO find return tyoe from code, documentation unavailable
+    parallels(value: [number, number]): any;
+    parallels(): [number, number];
+}
+
+export interface FeatureGenerator {
+    /**Returns a GeoJSON MultiLineString geometry object representing all meridians and parallels for this graticule. */
+    (): GeoJSON.MultiLineString;
+
+    lines(): GeoJSON.LineString[];
+    outline(): GeoJSON.Polygon;
+    extent(): [[number, number], [number, number]];
+    extent(extent: [[number, number], [number, number]]): this;
+    extentMajor(): [[number, number], [number, number]];
+    extentMajor(extent: [[number, number], [number, number]]): this;
+    extentMinor(): [[number, number], [number, number]];
+    extentMinor(extent: [[number, number], [number, number]]): this;
+    step(): [number, number];
+    step(step: [number, number]): this;
+    stepMajor(): [number, number];
+    stepMajor(step: [number, number]): this;
+    stepMinor(): [number, number];
+    precision(): number
+    precision(angle: number): this;
+}
+
 export interface ProjectionStream {
+}
+
+export interface Extent{
+    extent(): [[number, number], [number, number]];
+    extent(extent: [[number, number], [number, number]]): this;
+    stream(): ProjectionStream;
+    stream(value: ProjectionStream): this;
+}
+
+export interface Rotation {
+    (point: [number, number]): [number, number];
+    invert(point: [number, number]): [number, number];
+}
+
+export interface GeoPath {
+    area(object: GeoJSON.Feature<any>): number;
+    bounds(object: GeoJSON.Feature<any>): [[number, number], [number, number]];
+    centroid(object: GeoJSON.Feature<any>): [number, number];
+    context(): Context | null;
+    context(context: Context | null): this;
+    projection(): Projection;
+    projection(projection: Projection): this;
+    pointRadius(): number;
+    pointRadius(value: number): this;
+    (object: GeoJSON.Feature<any>): string;
+    (object: GeoJSON.Feature<any>, ...args: any[]): string;
+}
+
+export interface Context {
+    arc(x: number, y: number, radius: number, startAngle: number, endAngle: number, anticlockwise?: boolean): void;
+    beginPath(): void;
+    closePath(): void;
+    lineTo(x: number, y: number): void;
+    moveTo(x: number, y: number): void;
 }
 
 export interface RawProjection {
@@ -50,80 +122,20 @@ export function geoInterpolate(a: [number, number], b: [number, number]): (t: nu
 /**Returns a rotation function for the given angles, which must be a two- or three-element array of numbers [lambda, phi, gamma] specifying the rotation angles in degrees about each spherical axis. */
 export function geoRotation(angles: [number, number] | [number, number, number]): Rotation;
 
-export interface Rotation {
-    (point: [number, number]): [number, number];
-    invert(point: [number, number]): [number, number];
-}
-
-
 // ----------------------------------------------------------------------
 // Spherical Shapes
 // ----------------------------------------------------------------------
 export function geoCircle(): CircleGenerator;
-
-export interface CircleGenerator {
-    /**Returns a new GeoJSON geometry object of type “Polygon” approximating a circle on the surface of a sphere, with the current center, radius and precision. */
-    (...args: any[]): GeoJSON.Polygon;
-    center(): ((...args: any[]) => [number, number]) | [number, number];
-    center(center: ((...args: any[]) => [number, number]) | [number, number]): this;
-    radius(): ((...args: any[]) => number) | number;
-    radius(radius: ((...args: any[]) => number) | number): this;
-    precision(): ((...args: any[]) => number) | number;
-    precision(precision: ((...args: any[]) => number) | number): this;
-}
-
 export function graticule(): FeatureGenerator;
-
-export interface FeatureGenerator {
-    /**Returns a GeoJSON MultiLineString geometry object representing all meridians and parallels for this graticule. */
-    (): GeoJSON.MultiLineString;
-
-    lines(): GeoJSON.LineString[];
-    outline(): GeoJSON.Polygon;
-    extent(): [[number, number], [number, number]];
-    extent(extent: [[number, number], [number, number]]): this;
-    extentMajor(): [[number, number], [number, number]];
-    extentMajor(extent: [[number, number], [number, number]]): this;
-    extentMinor(): [[number, number], [number, number]];
-    extentMinor(extent: [[number, number], [number, number]]): this;
-    step(): [number, number];
-    step(step: [number, number]): this;
-    stepMajor(): [number, number];
-    stepMajor(step: [number, number]): this;
-    stepMinor(): [number, number];
-    precision(): number
-    precision(angle: number): this;
-}
 
 // ----------------------------------------------------------------------
 // Projections
 // ----------------------------------------------------------------------
-
 export function geoPath(): GeoPath;
 
-export interface GeoPath {
-    area(object: GeoJSON.Feature<any>): number;
-    bounds(object: GeoJSON.Feature<any>): [[number, number], [number, number]];
-    centroid(object: GeoJSON.Feature<any>): [number, number];
-    context(): Context | null;
-    context(context: Context | null): this;
-    projection(): Projection;
-    projection(projection: Projection): this;
-    pointRadius(): number;
-    pointRadius(value: number): this;
-    (object: GeoJSON.Feature<any>): string;
-    (object: GeoJSON.Feature<any>, ...args: any[]): string;
-}
-
-export interface Context {
-    arc(x: number, y: number, radius: number, startAngle: number, endAngle: number, anticlockwise?: boolean): void;
-    beginPath(): void;
-    closePath(): void;
-    lineTo(x: number, y: number): void;
-    moveTo(x: number, y: number): void;
-}
-
 export function geoProjection(project: RawProjection): Projection;
+//TODO type factory properly
+export function geoProjectionMutator(factory: any): any;
 
 export function geoAzimuthalEqualAreaRaw(): RawProjection;
 export function geoAzimuthalEquidistantRaw(): RawProjection;
@@ -139,4 +151,16 @@ export function geoTransverseMercatorRaw(): RawProjection;
 
 export function geoAlbers(): Projection;
 export function geoAlbersUsa(): Projection;
+export function geoAzimuthalEqualArea(): Projection;
+export function geoAzimuthalEquidistant(): Projection;
+export function geoConicConformal(): Projection;
+export function geoConicEqualArea(): Projection;
+export function geoConicEquidistant(): Projection;
+export function geoEquirectangular(): Projection;
+export function geoGnomonic(): Projection;
 export function geoMercator(): Projection;
+export function geoOrthographic(): Projection;
+export function geoStereographic(): Projection;
+export function geoTransverseMercator(): Projection;
+
+export function geoClipExtent(): Extent;
