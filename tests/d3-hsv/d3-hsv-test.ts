75385374f8f8612b073dc16e2fdb7a8bdc265a00
Tom Wanzek <tomwanzek@gmail.com>
2016-08-04 13:38:23 -0400

    chores: * Added definition and test file skeletons for the optional D3 modules: d3-geo-projection, d3-hexbin, d3-hsv, d3-sankey  and d3-tile. Fixes #82. * d3-timer: Fixed missing space in DefinitionsBy header.

---
+++ tests/d3-hsv/d3-hsv-test.ts
@@ -0,0 +1,10 @@
+/**
+ * Typescript definition tests for d3/d3-hsv module
+ *
+ * Note: These tests are intended to test the definitions only
+ * in the sense of typing and call signature consistency. They
+ * are not intended as functional tests.
+ */
+
+// TODO: uncomment below and add tests
+// import * as d3Hsv from '../../src/d3-hsv';
---
+++ tests/d3-sankey/d3-sankey-test.ts
@@ -0,0 +1,10 @@
+/**
+ * Typescript definition tests for d3/d3-sankey module
+ *
+ * Note: These tests are intended to test the definitions only
+ * in the sense of typing and call signature consistency. They
+ * are not intended as functional tests.
+ */
+
+// TODO: uncomment below and add tests
+// import * as d3Sankey from '../../src/d3-sankey';
