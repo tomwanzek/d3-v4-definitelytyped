d5e3ac20147aea7b626752c1c0409b02a0caf2c6
Tom Wanzek <tomwanzek@gmail.com>
2016-08-09 21:42:10 -0400

    Chore: * set primary author of new version 4 definitions by module. All contributors are credited based on Definitions By comment on @types npm page for the respective module. This only affects package.json affects one package.json field.

--- src/d3-request/index.d.ts
+++ src/d3-request/index.d.ts
@@ -1,6 +1,6 @@
 // Type definitions for D3JS d3-request module 1.0.1
 // Project: https://github.com/d3/d3-request/
-// Definitions by: Alex Ford <https://github.com/gustavderdrache>, Boris Yankov <https://github.com/borisyankov>, Hugues Stefanski <https://github.com/Ledragon>, Tom Wanzek <https://github.com/tomwanzek>
+// Definitions by: Hugues Stefanski <https://github.com/Ledragon>, Alex Ford <https://github.com/gustavderdrache>, Boris Yankov <https://github.com/borisyankov>, Tom Wanzek <https://github.com/tomwanzek>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 import { DSVParsedArray, DSVRowString, DSVRowAny } from '../d3-dsv';
