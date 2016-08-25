207206304aff6deb5e8915c0b0f4941c4a064cef
Eetu Mäkelä <eetu.makela@aalto.fi>
2016-08-25 11:32:58 +0300

    add null/undefined union types to definitions so they can be used in projects that employ strict null checking

--- src/d3-selection-multi/index.d.ts
+++ src/d3-selection-multi/index.d.ts
@@ -11,7 +11,7 @@ import {Transition} from '../d3-transition';
 export type ValueMap<Element, Datum> = { [key: string]: number | string | boolean | null | ValueFn<Element, Datum, number | string | boolean | null> };
 
 declare module '../d3-selection' {
-    export interface Selection<GElement extends BaseType, Datum, PElement extends BaseType, PDatum> {
+    export interface Selection<GElement extends BaseType | null, Datum, PElement extends BaseType | null, PDatum> {
         /**
          * Set multiple attributes on the given selection. Attribute values may be constant or derived from each node and its bound data.
          *
@@ -59,7 +59,7 @@ declare module '../d3-selection' {
 }
 
 declare module '../d3-transition' {
-    export interface Transition<GElement extends BaseType, Datum, PElement extends BaseType, PDatum> {
+    export interface Transition<GElement extends BaseType | null, Datum, PElement extends BaseType | null, PDatum> {
         /**
          * Set multiple attribute values. The transition will animate from the present value to the new value. Attribute values may be constant or derived from each node and its bound data.
          *
