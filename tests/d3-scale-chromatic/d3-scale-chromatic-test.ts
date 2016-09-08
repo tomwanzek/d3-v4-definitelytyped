590eeb110078bc28b9e448d13ececf54e0a6c373
Hugues Stefanski <hugues.stefanski@gmail.com>
2016-09-08 19:36:15 +0200

    d3-scale-chromatic tests started

--- tests/d3-scale-chromatic/d3-scale-chromatic-test.ts
+++ tests/d3-scale-chromatic/d3-scale-chromatic-test.ts
@@ -7,4 +7,7 @@
  */
 
 // TODO: uncomment below and add tests
-// import * as d3ScaleChromatic from '../../src/d3-scale-chromatic';
+import * as d3ScaleChromatic from '../../src/d3-scale-chromatic';
+
+let color1: string = d3ScaleChromatic.schemeAccent[0]; //rgb(127, 201, 127)
+let color2: string = d3ScaleChromatic.schemeDark2[0]; //rgb(27, 158, 119)
